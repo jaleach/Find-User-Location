ada8b01478ffaaae80895abf84ab0a80bcaf5d4f
jim leach <yogajim@gmail.com>
2017-01-05 22:30:33 -0500

    Initial Commit

---
+++ Finding The User Location/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  Finding The User Location
+//
+//  Created by James Leach on 1/5/17.
+//  Copyright © 2017 Dadio. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        
+//        let latitude = userLocation.coordinate.latitude
+//        
+//        let longitude = userLocation.coordinate.longitude
+//        
+//        let latDelta: CLLocationDegrees = 0.05
+//        
+//        let lonDelta: CLLocationDegrees = 0.05
+        
+//        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+//        for this
+//        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            
+            if error != nil {
+                print(error)
+                
+            } else {
+                
+               if let placemark = placemarks?[0] {
+                
+                var subThoroughfare = ""
+                
+                if placemark.subThoroughfare != nil {
+                    
+                    subThoroughfare = placemark.subThoroughfare!
+                    
+                }
+                
+                var thoroughfare = ""
+                
+                if placemark.thoroughfare != nil {
+                
+                thoroughfare = placemark.thoroughfare!
+                
+                }
+                
+                var subLocality = ""
+                
+                if placemark.subLocality != nil {
+                    
+                subLocality = placemark.subLocality!
+                    
+                }
+                
+                var subAdministrativeArea = ""
+                
+                if placemark.subAdministrativeArea != nil {
+                    
+                subAdministrativeArea = placemark.subAdministrativeArea!
+                    
+                }
+                var postalCode = ""
+                
+                if placemark.postalCode != nil {
+                    
+                    postalCode = placemark.postalCode!
+                    
+                }
+                var country = ""
+                
+                if placemark.country != nil {
+                    
+                    country = placemark.country!
+                    
+                }
+                 print(subThoroughfare + thoroughfare + "\n" + subLocality + "\n" + subAdministrativeArea + "\n" + postalCode + "\n" + country)
+                }
+                
+                
+                
+            }
+        }
+        
+//        let region = MKCoordinateRegion(center: location, span: span)
+//        
+//        self.map.setRegion(region, animated: true)
+//        
+    }
+    
+    
+}
+
